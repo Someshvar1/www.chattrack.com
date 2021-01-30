c19f7f06a262527f4a919e5681b436081e2199e0
Someshvar Sharan <75926693+Someshvar1@users.noreply.github.com>
2021-01-30 15:12:15 +0530

    Add files via upload

--- chatTrack_room.js
+++ chatTrack_room.js
@@ -13,22 +13,32 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-    
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-      
-      //End code
-      });});}
-getData();
+
 function addRoom(){
       roomname=document.getElementById("room_name").value;
       firebase.database().ref("/").child(roomname).update({
           purpose:"adding room name"
       });
       localStorage.setItem("roomname",roomname);
+      window.location="chattrackpage.html"
   }
   function logout(){
         localStorage.setItem("username","");
         window.location="index.html";
   }
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+      Room_names = childKey;
+     //Start code
+     console.log("Room name-"+Room_names);
+     row="<div class='room_name' id="+Room_names+">#"+Room_names+"</div><hr>";
+     document.getElementById("output").innerHTML+=row;
+     //End code
+     });});}
+getData();
+
+
+function redirectToRoomName(name){
+     console.log(name);
+     localStorage.setItem("room_name",name);
+     window.location="chattrack_page.html";
+}
